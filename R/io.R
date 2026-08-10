24a52d850d120b2f90cc750db68aa410f05266a0
Miles Smith <mileschristiansmith@gmail.com>
2026-08-10 15:21:51 -0500

    add unit tests for io.R

--- R/io.R
+++ R/io.R
@@ -13,32 +13,54 @@
 #'
 #' @returns A named list of tibbles
 #'
-.reader_func <- function(directory, file_extension = c("parquet", "excel")) {
+.reader_func <- function(
+  directory,
+  file_extension = c("parquet", "excel"),
+  sample_column = NULL,
+  project_column = NULL,
+  manifest_sheet = 1
+) {
+  if (!is.null(sample_column)) {
+    sample_column <- rlang::sym(sample_column)
+  }
+  if (!is.null(project_column)) {
+    project_column <- rlang::sym(project_column)
+  }
+
   file_extension <- match.arg(file_extension, several.ok = FALSE)
   # filter on the regex ^[[:alnum:]] to stop from loading
   # Excel's temporary files
 
   if (file_extension == "parquet") {
     pattern <- "\\.parquet$"
+    ext <- "parquet"
     read_func <- arrow::read_parquet
   } else if (file_extension == "excel") {
     pattern <- "^[[:alnum:]].*\\.xlsx"
+    ext <- "xlsx"
     read_func <- \(x) {
       {
         readxl::read_excel(
           path = x,
-          sheet = 1
+          sheet = manifest_sheet
         )
       } |>
-        dplyr::select(sample_id = Assay_Sample_ID, Project)
+        dplyr::select(
+          sample_id = {{ sample_column }},
+          project = {{ project_column }}
+        )
     }
   } else if (file_extension == "csv") {
     pattern <- "^[[:alnum:]].*\\.csv"
+    ext <- "csv"
     read_func <- \(x) {
       readr::read_csv(
         file = x,
-        col_select = c(sample_id = Assay_Sample_ID, Project)
-      )
+      ) |>
+        dplyr::select(
+          sample_id = {{ sample_column }},
+          project = {{ project_column }}
+        )
     }
   } else {
     stop("File must be either in parquet, excel, or comma-delimited format")
@@ -61,7 +83,7 @@
     pattern = .Platform$file.sep,
     i = -1
   ) |>
-    stringr::str_remove(pattern = paste(".", file_extension, sep = ""))
+    stringr::str_remove(pattern = paste(".", ext, sep = ""))
 
   data
 }
@@ -81,50 +103,81 @@ ingest_olink_data <- function(directory) {
 #' @title ingest_manifest
 #' @description Read in the manifest of samples for an Olink run \
 #'
-#' @param directory path to where Olink run manifest
+#' @param directory path to where Olink run manifest. Manifest MUST BE in excel format
+#' and MUST HAVE a `sample_id` and `project` columns.
+#' @param manifest_sheet Name of the sheet in the excel file containing the relevant sample information
+#' @param sample_column Name of column containing sample names that matches the values in `SampleID` in the Olink output. Default = `"sample_id"`
+#' @param project_column Name of the column containing the name of the project the sample is associated with. Default = `"project"`
+#' @param ... not currently used
 #'
-#' @returns A named list of tibbles
+#' @returns A named list of [`"tibbles"`][`tibble::tibble`]
+#'
 #' @export
 #'
-ingest_manifest <- function(directory) {
-  .reader_func(directory, file_extension = "excel")
+ingest_manifest <- function(
+  directory,
+  sample_column = "sample_id",
+  project_column = "project",
+  manifest_sheet = "ManifestBuilder",
+  ...
+) {
+  .reader_func(
+    directory = directory,
+    file_extension = "excel",
+    sample_column = sample_column,
+    project_column = project_column,
+    manifest_sheet = manifest_sheet,
+    ...
+  )
 }
 
 
 #' @title multifile_write
 #' @description Write multiple files into a list and name them based on the file name without file extension
 #'
-#' @param data A named list of [tibble::tibble]
+#' @param data A named list of [`tibble::tibble`]
 #' @param file_extension Output type: `"parquet"` or `"csv"` (default `"parquet"`)
 #'
 #' @importFrom purrr imap
 #' @importFrom arrow write_parquet
 #' @importFrom readr write_csv
+#' @importFrom stringr str_glue
 #'
 #' @returns Nothing
 #'
 #' @export
 #' @examples
-multifile_write <- function(data, file_extension = c("parquet", "csv")) {
+multifile_write <- function(
+  data,
+  file_extension = c("parquet", "csv"),
+  proj_dir = NULL
+) {
   file_extension <- match.arg(file_extension)
 
+  proj_dir <- proj_dir %||% getwd()
+  out_dir <- stringr::str_glue("{proj_dir}/SDP/Level_1")
+
+  if (!dir.exists(out_dir)) {
+    dir.create(out_dir, recursive = TRUE)
+  }
+
   if (file_extension == "parquet") {
-    purrr::imap(
+    purrr::iwalk(
       .x = data,
       .f = \(x, name) {
         arrow::write_parquet(
           x,
-          sink = paste0("SDP/Level_1/", name, ".parquet")
+          sink = stringr::str_glue("{out_dir}/{name}.parquet")
         )
       }
     )
   } else if (file_extension == "csv") {
-    purrr::imap(
+    purrr::iwalk(
       .x = data,
-      .f = \(x) {
+      .f = \(x, name) {
         readr::write_csv(
           x = x,
-          file = paste0("SDP/Level_1/", x, ".csv")
+          file = stringr::str_glue("{out_dir}/{name}.csv")
         )
       }
     )
---
+++ tests/testthat/test-io.R
@@ -0,0 +1,75 @@
+testthat::test_that(desc = "ingest_olink_data", code = {
+  expected_data <- readRDS(here::here(
+    "tests",
+    "testthat",
+    "data",
+    "io",
+    "ingest_olink_data_expected.RDS"
+  ))
+  test_data_directory <- here::here("tests", "initial_data")
+  testthat::expect_mapequal(
+    olinkqc::ingest_olink_data(directory = test_data_directory),
+    expected_data
+  )
+})
+
+testthat::test_that(desc = "ingest_manifest", code = {
+  expected_manifest <- readRDS(here::here(
+    "tests",
+    "testthat",
+    "data",
+    "io",
+    "ingest_manifest_expected.RDS"
+  ))
+  test_data_directory <- here::here("tests", "initial_data")
+  testthat::expect_mapequal(
+    object = olinkqc::ingest_manifest(
+      directory = test_data_directory,
+      sample_column = "Tube ID",
+      project_column = "Project",
+      manifest_sheet = "ManifestBuilder"
+    ),
+    expected = expected_manifest
+  )
+})
+
+testthat::test_that(desc = "multifile_write", code = {
+  expected_data <- readRDS(here::here(
+    "tests",
+    "testthat",
+    "data",
+    "io",
+    "ingest_olink_data_expected.RDS"
+  ))
+  expected_manifest <- readRDS(here::here(
+    "tests",
+    "testthat",
+    "data",
+    "io",
+    "ingest_manifest_expected.RDS"
+  ))
+  path <- withr::local_tempfile()
+  multifile_write(expected_data, file_extension = "parquet", proj_dir = path)
+  multifile_write(expected_manifest, file_extension = "csv", proj_dir = path)
+  print(list.files(path))
+  testthat::expect_snapshot_file(
+    stringr::str_glue(
+      "{path}/SDP/Level_1/FS19030710_NPX_2026-06-22.parquet"
+    )
+  )
+  testthat::expect_snapshot_file(
+    stringr::str_glue(
+      "{path}/SDP/Level_1/FS19030718_NPX_2026-06-26.parquet"
+    )
+  )
+  testthat::expect_snapshot_file(
+    stringr::str_glue(
+      "{path}/SDP/Level_1/FS19030751_NPX_2026-06-22.parquet"
+    )
+  )
+  testthat::expect_snapshot_file(
+    stringr::str_glue(
+      "{path}/SDP/Level_1/2026-05-15 James ABC-PreEVADE_Peds Serum_Olink (C.Guthridge)_Manifest.csv"
+    )
+  )
+})
